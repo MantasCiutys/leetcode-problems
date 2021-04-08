1e20891e94d28d23b10831baaa838a967a2750be
Mantas Ciutys <ciutysmant@gmail.com>
2021-04-08 17:18:31 +0100

    Memoized Coin Change solution implemented

--- src/DynamicProgramming/CoinChange.java
+++ src/DynamicProgramming/CoinChange.java
@@ -1,4 +1,8 @@
 package DynamicProgramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 You are given an integer array coins representing coins of different denominations and an integer amount representing
 a total amount of money.
@@ -9,7 +13,7 @@ combination of the coins, return -1.
 You may assume that you have an infinite number of each kind of coin.
  */
 public class CoinChange {
-    private static int coinChange(int[] coins, int amount) {
+    private static int naiveCoinChange(int[] coins, int amount) {
         if (amount < 0) return -1;
         if (amount == 0) return 0;
 
@@ -17,7 +21,7 @@ public class CoinChange {
 
         for (int coin : coins) {
             int leftOver = amount - coin;
-            int childCoinChange = coinChange(coins, leftOver);
+            int childCoinChange = naiveCoinChange(coins, leftOver);
 
             // Impossible for this child to get amount
             if (childCoinChange != -1) {
@@ -36,16 +40,54 @@ public class CoinChange {
         return minCoins;
     }
 
+    private static int memoCoinChange(int[] coins, int amount) {
+        return memoCoinChangeHelper(coins, amount, new HashMap<>());
+    }
+
+    private static int memoCoinChangeHelper(int[] coins, int amount, Map<Integer, Integer> map) {
+        if (map.containsKey(amount)) return map.get(amount);
+        if (amount < 0) return -1;
+        if (amount == 0) return 0;
+
+        int minCoins = Integer.MAX_VALUE;
+
+        for (int coin : coins) {
+            int leftOver = amount - coin;
+            int childCoinChange = memoCoinChangeHelper(coins, leftOver, map);
+
+            // Impossible for this child to get amount
+            if (childCoinChange != -1) {
+                childCoinChange++;
+            }
+
+            // Update minimum if child coin change min is not -1 only
+            if (childCoinChange != -1) {
+                minCoins = Math.min(minCoins, childCoinChange);
+            }
+
+        }
+
+        // if suitable coin not found, then not possible to get amount, return -1
+        if (minCoins == Integer.MAX_VALUE) {
+            map.put(amount, -1);
+            return -1;
+        }
+        map.put(amount, minCoins);
+        return minCoins;
+    }
+
     public static void main(String[] args) {
-        //int[] coins = {1, 2, 5};
-        //int amount = 11;
+        int[] coins = {1, 2, 5};
+        int amount = 11;
         //int[] coins = {2};
         //int amount = 3;
 
-        int[] coins = {1, 2, 5};
-        int amount = 100;
+        //int[] coins = {1, 2, 5};
+        //int amount = 100;
 
-        int naiveOutput = coinChange(coins, amount);
-        System.out.println("Naive output: " + naiveOutput);
+        //int naiveOutput = naiveCoinChange(coins, amount);
+        int memoOutput = memoCoinChange(coins, amount);
+
+        System.out.println("Memoized output: " + memoOutput);
     }
 }
