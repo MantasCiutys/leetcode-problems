2cba6ee6900ab55a1f5ec57b36363308db168cbd
Mantas Ciutys <ciutysmant@gmail.com>
2021-04-18 14:00:26 +0100

    Tabulated approach for Coin Change 2 problem - (incorrect)

--- src/DynamicProgramming/CoinChange2.java
+++ src/DynamicProgramming/CoinChange2.java
@@ -1,4 +1,7 @@
 package DynamicProgramming;
+
+import java.util.Arrays;
+
 /*
 You are given coins of different denominations and a total amount of money. Write a function to compute the number of
 combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
@@ -19,15 +22,33 @@ public class CoinChange2 {
         return totalCombinations;
     }
 
-    public static void main(String[] args) {
-        //int[] coins = {1, 2, 5};
-        //int amount = 5;
+    private static int tabulatedChange(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, 0);
 
-        int[] coins = {2};
-        int amount = 3;
+        dp[0] = 1;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                int index = i - coins[j];
+                if (index >= 0) dp[i] += dp[i - coins[j]];
+            }
+        }
+
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 5;
+
+        //int[] coins = {2};
+        //int amount = 3;
 
         int naiveOutput = naiveChange(amount, coins);
+        int tabulatedApproach = tabulatedChange(amount, coins);
 
         System.out.println("Naive output: " + naiveOutput);
+        System.out.println("Tabulated output: " + tabulatedApproach);
     }
 }
